eba887edd66cccfaf5aae06ff1a33c299e175f64
Flávia Guterres <guterres@gmail.com>
2019-01-25 19:39:46 +0000

    Resolução do desafio básico da aula 013

---
+++ loja-fone/js/button-store.js
@@ -0,0 +1,9 @@
+/* 
+1 - Pegar o elemento "Adiciona no carrinho" e colocar no console.
+2 - Quando clicar no elemento "Adiciona no carrinho", deve aparecer uma mensagem de texto.
+3 - Quando clicar no elemento "Adiciona no carrinho", modificar o elemento "Carrinho (10)". Não precisa mudar a numeração, pode mudar somente o texto.
+*/
+
+const $second = window.document.querySelector(".-second");
+
+console.log($second);
